a812dbed464c1442f168668c04727dd78805d8d3
Jonathon Poff <poffjl@gmail.com>
2018-04-27 17:32:24 -0400

    Hello SKRYPT!

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello SKRYPT!');
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
