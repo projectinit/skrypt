aef30626281f5f25b23f897e6aeef143dcf6dc5f
Jonathon Poff <poffjl@gmail.com>
2018-04-27 19:34:23 -0400

    required postgres, commented pg init for now

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+//const pgp = require('pg-promise')(/*options*/);
+//const db = pgp('postgres://username:password@host:port/database');
 
 app.use(express.static('public'));
 
